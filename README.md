352a7aafcbe10d8834e96d515156db84327ddd56
Dan Kelosky <dkelosky@gmail.com>
2024-02-22 10:46:53 -0500

    webpack
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+#
+
+## Dev
+
+`npx tsc` outputs `*.mjs` files to a `lib` folder.  However, we can take `npx webpack` to output files from typescript to `dist` folder.
+
+For now, `lib` folder will not be used.
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    //   entry: './lib/index.mjs', // builds dependency graph from this
+    entry: './src/index.mts', // builds dependency graph from this
+    module: {
+        rules: [{
+
+            use: 'ts-loader',
+            exclude: '/node_modules/'
+        }]
+    },
+    mode: 'development', // could be production which minifies everything
+    output: {
+        filename: 'view.bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
